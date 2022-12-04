25b67bb2479d2cbb88f0e78538c8b3ecb03ba93b
sangwon <swchoi1997@naver.com>
2022-12-04 13:54:28 +0900

    first-commit

---
+++ src/main/java/com/d/caffeination/DCaffeinationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.d.caffeination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DCaffeinationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DCaffeinationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d/caffeination/DCaffeinationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d.caffeination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DCaffeinationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
